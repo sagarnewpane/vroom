5d867ad70289a3e12e9859ecf9b5b677e3f22664
Sagar Newpane <sdreamnewpane@gmail.com>
2024-04-06 14:38:14 +0545

    added js for form in home and bookings in seperate files

--- vroom/static/Vroom.js
+++ vroom/static/Vroom.js
@@ -11,3 +11,8 @@ document.getElementById('carForm').addEventListener('submit', function(e) {
 
     this.submit();
 });
+
+flatpickr(".flatpickr", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+});
