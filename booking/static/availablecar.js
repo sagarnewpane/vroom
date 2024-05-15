d6ff44ac2b646d2e18165f068fefc162cb1e733f
Sagar Newpane <sdreamnewpane@gmail.com>
2024-05-15 17:40:38 +0545

    made filter permanent on larger display

--- booking/static/availablecar.js
+++ booking/static/availablecar.js
@@ -1,17 +1,17 @@
 // JavaScript function to toggle filter menu
 function toggleFilterMenu() {
-    var menu = document.getElementById('filter-menu');
-    if (menu.style.display === '' || menu.style.display === 'none') {
-        menu.style.display = 'block';
-        document.querySelector('.car-container').style.flexGrow = '0.8';
-    } else {
-        menu.style.display = 'none';
-        document.querySelector('.car-container').style.flexGrow = '1';
-    }
+  var menu = document.getElementById("filter-menu");
+  if (menu.style.display === "" || menu.style.display === "none") {
+    menu.style.display = "block";
+    document.querySelector(".car-container").style.flexGrow = "0.8";
+  } else {
+    menu.style.display = "none";
+    document.querySelector(".car-container").style.flexGrow = "1";
+  }
 }
 
-// Set the initial display property of the filter menu
-window.onload = function() {
-    var menu = document.getElementById('filter-menu');
-    menu.style.display = 'none';
-}
+// // Set the initial display property of the filter menu
+// window.onload = function () {
+//   var menu = document.getElementById("filter-menu");
+//   menu.style.display = "none";
+// };
